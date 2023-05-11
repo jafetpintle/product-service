f78ba3c091ce6656ed63f835703c190852c31f78
jafet <jafetpintle@gmail.com>
2023-05-10 22:37:09 -0600

    Adding getters and setters instead LOMBOK sintaxis

--- src/main/java/com/japy/productmicroservice/Entity/ProductEntity.java
+++ src/main/java/com/japy/productmicroservice/Entity/ProductEntity.java
@@ -1,14 +1,11 @@
 package com.japy.productmicroservice.Entity;
 
-import lombok.Getter;
+
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(value = "product")
-@Getter
-@Setter
 @NoArgsConstructor
 public class ProductEntity {
     @Id
@@ -17,4 +14,35 @@ public class ProductEntity {
     private String description;
     private Double unitPrice;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(Double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
 }
