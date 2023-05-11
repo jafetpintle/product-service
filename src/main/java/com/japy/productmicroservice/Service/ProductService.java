b82617a13bbafdda9ac28e2492df97c923f0cc98
jafet <jafetpintle@gmail.com>
2023-05-10 22:01:05 -0600

    Product service structure

--- src/main/java/com/japy/productmicroservice/Service/ProductService.java
+++ src/main/java/com/japy/productmicroservice/Service/ProductService.java
@@ -1,4 +1,15 @@
 package com.japy.productmicroservice.Service;
 
+import com.japy.productmicroservice.Repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ProductService {
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+
 }
