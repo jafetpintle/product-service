aa30d3e7232d297f510d8bb006b023ab8a6df857
jafet <jafetpintle@gmail.com>
2023-05-10 22:38:14 -0600

    Adding service methods from product repository

--- src/main/java/com/japy/productmicroservice/Service/ProductService.java
+++ src/main/java/com/japy/productmicroservice/Service/ProductService.java
@@ -1,8 +1,14 @@
 package com.japy.productmicroservice.Service;
 
+import com.japy.productmicroservice.Entity.ProductEntity;
 import com.japy.productmicroservice.Repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ProductService {
     private final ProductRepository productRepository;
 
@@ -11,5 +17,23 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
+    public List<ProductEntity> findAll() {
+        return productRepository.findAll();
+    }
 
+    public Optional<ProductEntity> findById(String s) {
+        return productRepository.findById(s);
+    }
+
+    public ProductEntity save(ProductEntity entity) {
+        return productRepository.save(entity);
+    }
+
+    public Iterable<ProductEntity> findAllById(Iterable<String> strings) {
+        return productRepository.findAllById(strings);
+    }
+
+    public void delete(ProductEntity entity) {
+        productRepository.delete(entity);
+    }
 }
